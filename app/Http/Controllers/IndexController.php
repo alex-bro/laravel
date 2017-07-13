0712a70db50f228870d5fd5ef50bb557b7cfee2b
alex-bro <alexbronnikov76@gmail.com>
2017-07-13 16:43:37 +0300

    first lesson 5

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\People;
+
+class IndexController extends Controller
+{
+    //
+    public function execute(Request $request){
+
+        $pages = Page::all();
+        $portfolios = Portfolio::get(['name', 'filter', 'images']);
+        $services = Service::where('id','<',20)->get();
+        $peoples = People::take(3)->get();
+
+
+        return view('site.index');
+    }
+}
